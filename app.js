a6a6e53169cfed640aa1b313271c47236f182a52
mahfuz hussian <mahfuz477@gmail.com>
2023-08-24 01:33:24 +0100

    update mask to clippath

--- app.js
+++ app.js
@@ -7,84 +7,80 @@ const svg = d3
   .attr("width", width)
   .attr("height", height);
 
-let gridMovment = 0;
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${width / 2} ${height / 2})`);
 
-const renderMask = (selection, id, rotate, moveright) => {
-  svg
-    .append("g")
-    .attr("transform", `translate(${moveright} 0)`)
-    .attr("width", width)
-    .attr("height", height)
-    .selectAll("rect")
-    .data(d3.range(22))
-    .join("rect")
-    .attr("x", (d) => (d == 0 ? d : d * 22.68 - 3.78))
-    .attr("width", 3.78)
-    .attr("height", height)
-    .attr("fill", "black")
-    .attr("mask", `url(#${id})`);
-  const mask = selection.append("mask").attr("id", id);
-
-  mask
-    .append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "black");
-  const g = mask
-    .append("g")
-    .attr(
-      "transform",
-      `translate(${width / 2},${height / 2}) rotate(${rotate})`
-    );
-
-  g.selectAll("path")
-    .data(d3.range(1))
+const renderStar = (id, moveclip, rotateStar) => {
+  const mask1r = g.append("g");
+  mask1r
+    .append("path")
+    .data([null])
     .join("path")
     .attr("d", (d) => d3.symbol(d3.symbols[4], 30000)())
-    .attr("fill", "white");
+    .attr("transform", `rotate(${-rotateStar})`)
+    .attr("fill", "black")
+    .attr("clip-path", `url(#${id})`);
+
+  mask1r
+    .append("clipPath")
+    .attr("id", id)
+    .selectAll("rect")
+    .data(d3.range(50))
+    .join("rect")
+    .attr("width", 3.78)
+    .attr("height", 350)
+    .attr("x", (d) => d * 22.68 - 3.78 - width / 2 / 2 + moveclip)
+    .attr("y", -width / 2 / 2 -25)
+    .attr("fill", "white")
+    .attr("transform", `rotate(${rotateStar})`);
 };
 
 let rotateStar = 0;
-let moveright = 0;
+let moveclip = 0;
 
-//width determine by totalframe eg: 6 star frame width = 0.1cm(3.78px) and maingrid width = o.5cm(18.90px)
+// width determine by totalframe eg: 6 star frame width = 0.1cm(3.78px) and maingrid width = o.5cm(18.90px)
 
 for (let frame = 0; frame < 6; frame++) {
   if (frame === 1) {
     rotateStar = 60;
   }
-  svg.call(renderMask, "frame" + frame, rotateStar, moveright);
-  rotateStar = rotateStar * 2;
-  moveright = moveright + 3.78;
+  renderStar("frame" + frame, moveclip, rotateStar);
+  rotateStar = rotateStar + 60;
+  moveclip = moveclip + 3.78;
 }
 
-const g = svg
+  let gridMovement = -400
+
+svg
   .append("g")
   .attr("id", "maingrid")
-  .attr("transform", `translate(${gridMovment} 0)`)
+  .attr("transform", `translate(${gridMovement} 0)`)
   .attr("width", width)
   .attr("height", height)
   .selectAll("rect")
   .data(d3.range(22))
   .join("rect")
-  .attr("x", (d) => d * 22.68)
+  .attr("x", (d) => d * 22.68 - 3.78)
   .attr("width", 18.9)
   .attr("height", height)
-  .attr("fill", "black")
-  .attr("opacity", "1");
+  .attr("fill", "black");
+
 
 let moveleft = false;
 
 setInterval(() => {
-  if (gridMovment === 0) {
+  if (gridMovement === -300 ) {
     moveleft = false;
-  } else if (gridMovment === 100) {
+  } else if (gridMovement === 300) {
     moveleft = true;
   }
   if (moveleft === false) {
-    gridMovment = gridMovment + 1;
+    gridMovement = gridMovement + 2;
   } else if (moveleft === true) {
-    gridMovment = gridMovment - 1;
+    gridMovement = gridMovement - 2;
   }
-  d3.select("#maingrid").attr("transform", `translate(${gridMovment} 0)`);
-}, 100);
+  d3.select("#maingrid").attr("transform", `translate(${gridMovement} 0)`);
+}, 50);
+
+
